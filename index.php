f27c19ff0088ea238f8f1d5ccac83a0cf061be68
Elsadora <89636417+Elsadora@users.noreply.github.com>
2022-04-15 00:22:41 +0700

    update 3

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <title>Index Page Dinamis 1</title>
 
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" /> -->
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"/>
 
